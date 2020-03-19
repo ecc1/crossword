bfe78792d36c56f73403e76181e46337659a10e2
Eric Cooper <ecc@cmu.edu>
2020-03-18 23:07:24 -0400

    Add rendering tests

---
+++ render_test.go
@@ -0,0 +1,27 @@
+package acrosslite
+
+import (
+	"path"
+	"testing"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func TestRenderAllPuzzles(t *testing.T) {
+	for _, file := range testFiles() {
+		base := path.Base(file)
+		t.Run(base, func(t *testing.T) {
+			p, err := Read(file)
+			if err != nil {
+				t.Errorf("%s", err)
+				return
+			}
+			pdf := gofpdf.New("L", "pt", "Letter", "")
+			p.Render(pdf)
+			pdf.Close()
+			if pdf.PageCount() != 1 {
+				t.Errorf("Render(%s) produced %d pages", base, pdf.PageCount())
+			}
+		})
+	}
+}
