87185bed4e3e5b52b8b459c2149e652e58b8f098
Eric Cooper <ecc@cmu.edu>
2020-03-18 23:13:13 -0400

    Add puz2pdf program

---
+++ cmd/puz2pdf/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/ecc1/acrosslite"
+	"github.com/jung-kurt/gofpdf"
+)
+
+var (
+	forceFlag  = flag.Bool("f", false, "force overwriting of PDF output file")
+	outputFile = flag.String("o", "", "write PDF output to `file`")
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fail(fmt.Errorf("no input files"))
+	}
+	pdf, err := setupOutput()
+	if err != nil {
+		fail(err)
+	}
+	for _, file := range flag.Args() {
+		err := puz2pdf(file, pdf)
+		if err != nil {
+			fail(err)
+		}
+	}
+	pdf.OutputFileAndClose(*outputFile)
+}
+
+func setupOutput() (*gofpdf.Fpdf, error) {
+	if *outputFile == "" {
+		if flag.NArg() > 1 {
+			return nil, fmt.Errorf("multiple input files; output file must be specified with \"-o\"")
+		}
+		file := flag.Arg(0)
+		base := path.Base(file)
+		ext := path.Ext(base)
+		if ext != ".puz" {
+			return nil, fmt.Errorf("%s: file name must end with .puz", file)
+		}
+		*outputFile = base[:len(base)-len(ext)] + ".pdf"
+	}
+	if exists(*outputFile) && !*forceFlag {
+		return nil, fmt.Errorf("output file %s already exists; use \"-f\" to overwrite", *outputFile)
+	}
+	return gofpdf.New("L", "pt", "Letter", ""), nil
+}
+
+func puz2pdf(file string, pdf *gofpdf.Fpdf) error {
+	puz, err := acrosslite.Read(file)
+	if err != nil {
+		return err
+	}
+	puz.Render(pdf)
+	return nil
+}
+
+func exists(file string) bool {
+	_, err := os.Stat(file)
+	return !os.IsNotExist(err)
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
