d1be910cca486bf174b1049e806e68eba9f99d97
Eric Cooper <ecc@cmu.edu>
2020-03-18 23:02:15 -0400

    Add -g flag for Go struct format

--- cmd/readpuz/main.go
+++ cmd/readpuz/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/ecc1/acrosslite"
 )
 
+var (
+	goFormat = flag.Bool("g", false, "print puzzle in Go struct format")
+)
+
 func main() {
-	p, err := acrosslite.Read(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
-		os.Exit(1)
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fail(fmt.Errorf("single PUZ file required"))
 	}
-	fmt.Printf("%+v\n", *p)
+	p, err := acrosslite.Read(flag.Arg(0))
+	if err != nil {
+		fail(err)
+	}
+	if *goFormat {
+		fmt.Printf("%#v\n", *p)
+	} else {
+		fmt.Printf("%+v\n", *p)
+	}
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
+	os.Exit(1)
 }
