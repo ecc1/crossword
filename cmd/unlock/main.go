a5f9187bff92ff80df50fb4a23c7f30e0398999b
Eric Cooper <ecc@cmu.edu>
2020-03-31 16:42:49 -0400

    Add unlock program

---
+++ cmd/unlock/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/ecc1/acrosslite"
+)
+
+func main() {
+	var file string
+	var key int
+	var err error
+	switch len(os.Args) {
+	case 2:
+		file = os.Args[1]
+	case 3:
+		file = os.Args[1]
+		key, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			fail(err)
+		}
+		if key == 0 {
+			fail(fmt.Errorf("0000 is not a valid key"))
+		}
+	default:
+		fail(fmt.Errorf("Usage: %s file.puz [key]", os.Args[0]))
+	}
+	puz, err := acrosslite.Read(file)
+	if err != nil {
+		fail(err)
+	}
+	if key != 0 {
+		err = puz.UnlockWithKey(key)
+	} else {
+		key, err = puz.Unlock()
+		if err == nil {
+			fmt.Printf("[key = %04d]\n", key)
+		}
+	}
+	if err != nil {
+		fail(err)
+	}
+	fmt.Print(puz.Solution())
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
